c578933394d6d55b43c3db687f672e341eeb9d0d
siva-hut <sivamigi7@gmail.com>
2023-01-20 14:31:38 +0530

    new changes for merge conflict

--- src/dataTypes.java
+++ src/dataTypes.java
@@ -11,21 +11,7 @@ class test{
 public class dataTypes {
     public static void main(String args[])
     {
-        /*int s1 = 10;
-        Integer s2 = 10;
-        String s3 = "1234";
-        Integer s4 = Integer.parseInt(s3);
-        System.out.println(s4);
-        int [] arr1 = {10,20};
-        int [] arr2 = arr1;
-        arr2[0] = 20;
-        System.out.println(arr1[0] + " "+ arr2[0]);
-        arr1 = new int[10];
-        System.out.println((byte)arr1[0]);
-        boolean b1 = s2.getClass().equals(Integer.class);
-        System.out.println(b1);
-        final dataTypes d = new dataTypes();
-        */
+
         test t1 = new test();
     }
 }
