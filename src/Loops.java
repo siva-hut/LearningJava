89f3c1b913a03600814ae016bb9303916fa55907
siva-hut <sivamigi7@gmail.com>
2023-01-20 09:33:55 +0530

    Initial commit

---
+++ src/Loops.java
@@ -0,0 +1,21 @@
+public class Loops {
+    public static void main(String args[])
+    {
+        for (int i = 0; i < 10; i++) {
+            //simple for
+        }
+        int i = 0;
+        while(i<10)
+        {   //simple while
+            i++;
+        }
+        do{
+            //simple do while
+            i++;
+        }while (i<10);
+        char [] vowels = {'a','e'};
+        for (char item:vowels) {
+            //enhanced for
+        }
+    }
+}
