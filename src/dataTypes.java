086aa4c3c5a839cbdc573e9f189959f8595ad9a2
siva-hut <sivamigi7@gmail.com>
2023-01-20 14:16:31 +0530

    no comments

--- src/dataTypes.java
+++ src/dataTypes.java
@@ -11,7 +11,7 @@ class test{
 public class dataTypes {
     public static void main(String args[])
     {
-        /*int s1 = 10;
+        int s1 = 10;
         Integer s2 = 10;
         String s3 = "1234";
         Integer s4 = Integer.parseInt(s3);
@@ -24,7 +24,7 @@ public class dataTypes {
         System.out.println((byte)arr1[0]);
         boolean b1 = s2.getClass().equals(Integer.class);
         System.out.println(b1);
-        final dataTypes d = new dataTypes();*/
+        final dataTypes d = new dataTypes();
 
         test t1 = new test();
     }
