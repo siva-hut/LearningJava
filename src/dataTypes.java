ccec134751d3ee3a79079ace4a4b8b6e5ef8f421
siva-hut <sivamigi7@gmail.com>
2023-01-20 14:32:33 +0530

    new changes for merge conflict

--- src/dataTypes.java
+++ src/dataTypes.java
@@ -11,7 +11,7 @@ class test{
 public class dataTypes {
     public static void main(String args[])
     {
-        /*int s1 = 10;
+        int s1 = 10;
         Integer s2 = 10;
         String s3 = "1234";
         Integer s4 = Integer.parseInt(s3);
@@ -25,7 +25,7 @@ public class dataTypes {
         boolean b1 = s2.getClass().equals(Integer.class);
         System.out.println(b1);
         final dataTypes d = new dataTypes();
-        */
+
         test t1 = new test();
     }
 }
